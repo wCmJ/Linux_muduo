4d776da9d18de0ac0b1808f6d3c73205ae69b898
python <younext@126.com>
2019-11-04 16:43:23 +0800

    Update noncopyable_MutexLock_MutexLockGuard.cpp

--- noncopyable_MutexLock_MutexLockGuard.cpp
+++ noncopyable_MutexLock_MutexLockGuard.cpp
@@ -41,4 +41,25 @@ private:
     MutexLock& mutex_;
 };
 
+class Condition{
+    explicit Condition(MutexLock& mutex):mutex_(mutex){
+        pthread_cond_init(&pcond_, NULL);   
+    }
+    ~Condition(){
+        pthread_cond_destroy(&pcond_);   
+    }
+    void wait(){
+        pthread_cond_wait(&pcond_, mutex_.getPthreadMutex());
+    }
+    void notify(){
+        pthread_cond_signal(&pcond_);   
+    }
+    void notifyAll(){
+        pthread_cond_broadcast(&pcond_);   
+    }
+    
+private:    
+    MutexLock& mutex_;
+    pthread_cond_t pcond_;
+};
 
